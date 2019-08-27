e588fd73b02892df72a5ccb429b4d5da1e871531
someone <fullmetalgame12@gmail.com>
2019-08-27 20:52:00 +0300

    some unneccesary changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path'); //вебпак интерпритируется нодом, так что require работает(только в этом файле)
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // для раздельной компиляции css и js
+
+
 //entry -> output
 
 //тут нужно сказать webpack'у что ему транслировать(интерпритировать? компилировать? транспилировать?) и куда
-// и как в зависимости от того, продакш ли это или дев
+// и как, в зависимости от того, продакш ли это или дев
 
 module.exports = (env)=>{
     const isProduction = env === 'production';
@@ -18,29 +21,59 @@ module.exports = (env)=>{
         watch: true,
 
         module:{
-            rules:[{
-                //дружу webpack с babel
-                //говорю ему: хэй, вебпак,
-                loader: 'babel-loader', //через такую штуку
-                test: /\.js$/, //бабли такие файлы
-                exclude: [
-                    '/node_modules/', //но кроме тех, которые лежат здесь
-                    //'/src/playground/'//и эти тоже не бабли, ок?
-                ]
+            rules:[
 
-            },
                 {
-                    //говорю вебпаку грузить css через эти npm модули
+                    //дружу webpack с babel
+                    //говорю ему: хэй, вебпак,
+                    loader: 'babel-loader', //через такую штуку
+                    test: /\.js$/, //бабли такие файлы
+                    exclude: [
+                        '/node_modules/', //но кроме тех, которые лежат здесь
+                        //'/src/playground/'//и эти тоже не бабли, ок?
+                ]
+                },
+
+                {
+                    //говорю вебпаку грузить css через эти npm модули(стили будут лежать вместе с js в одном файле)
                     test: /\.s?css$/,
                     //use - массив loader'ов
-                    use: ['style-loader', 'css-loader', 'sass-loader']
+                    use: ['style-loader','css-loader', 'sass-loader']
                 },
+
+                //гружу стили в отдельный css файл
+                // {
+                //     test: /\.s?css$/,
+                //     //говорю вебпаку грузить css/scss через эти npm модули
+                //     use: [
+                //         MiniCssExtractPlugin.loader,
+                //         {
+                //             loader: "css-loader",
+                //             options: {
+                //                 modules: true,
+                //                 sourceMap: !isProduction,
+                //                 importLoaders: 2
+                //             }
+                //         },
+                //         "sass-loader"
+                //     ]
+                // }
             ]
         },
+        // plugins: [
+        //     new MiniCssExtractPlugin({
+        //         // Options similar to the same options in webpackOptions.output
+        //         // all options are optional
+        //         filename: 'styles.css',
+        //         chunkFilename: '[id].css',
+        //         ignoreOrder: false, // Enable to remove warnings about conflicting order
+        //     }),
+        // ],
+
         //cheap-module-eval-source-map для удобного дебага в браузере(вместо моего кода не билеберда вебпаковская),
         //для продакшена не пойдет, ибо медленное
         //source-map для продакшена будет грузится только при нажатии f12, а так страница весит 600кб
-        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devtool: isProduction ?  undefined : 'cheap-module-eval-source-map',
         mode: "development",
         //удобный сервак, который сразу запускает webpack
         //но при этом bundle у него не физический, т.е. лежит в оперативе, а не в файле, чтобы быстрее работало
